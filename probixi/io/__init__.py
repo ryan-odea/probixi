9354f3ea0078a65f89e3ad6c25dda91f7d916737
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2026-06-17 12:53:29 +0200

    init file

---
+++ probixi/io/__init__.py
@@ -0,0 +1,18 @@
+# read = place into memory, scan = lazy read
+from .cell import CellParams, read_crystfel_cell
+from .frames import DataLoader, iter_frames
+from .geometry import BadRegion, Geometry, read_geometry
+from .metadata import H5Info, Metadata, scan_h5
+
+__all__ = [
+    "CellParams",
+    "read_crystfel_cell",
+    "BadRegion",
+    "Geometry",
+    "read_geometry",
+    "DataLoader",
+    "iter_frames",
+    "H5Info",
+    "Metadata",
+    "scan_h5"
+]
