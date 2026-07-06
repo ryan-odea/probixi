0f8d6ba480c3578ec09c7aaedf955722c528bd13
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2026-07-06 11:44:38 +0200

    remake init

--- probixi/__init__.py
+++ probixi/__init__.py
@@ -1,1 +1,24 @@
-from .probixi import Probixi as Probixi
+from __future__ import annotations
+
+from .indexer import CellMatchConfig, IntegrateConfig, RefineConfig, SeedConfig
+from .io import DataOffloader, PeakOffloader
+from .multigpu import BlockConfig, merge_streams, run_block_from_env, run_data_parallel
+from .probixi import Probixi
+
+__all__ = [
+    # pipeline
+    "Probixi",
+    # indexer config
+    "SeedConfig",
+    "RefineConfig",
+    "CellMatchConfig",
+    "IntegrateConfig",
+    # multi-GPU
+    "run_data_parallel",
+    "run_block_from_env",
+    "merge_streams",
+    "BlockConfig",
+    # output writers
+    "DataOffloader",
+    "PeakOffloader",
+]
